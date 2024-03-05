cc72e15ae9cea2f61da1e7d6f16193bc2b929418
Sumanthbabu Muthineni <118552809+Sumanthbabu-Muthineni@users.noreply.github.com>
2024-03-05 22:29:38 +0530

    sends gmail upon registration

--- event_registration/registration_system/views.py
+++ event_registration/registration_system/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 from .forms import RegistrationForm
 from .models import Registration
+from django.conf import settings
+from django.core.mail import send_mail
 
 def registration_view(request):
     if request.method == 'POST':
@@ -21,6 +23,12 @@ def registration_view(request):
 
             # Save the form data to the database
             registration = form.save()
+              
+            subject = 'Welcome to the Grand Event!'
+            message = f'Hi {registration.full_name},\n\nThank you for registering for the Grand Event.\n\nWe look forward to seeing you there!\n\nBest regards,\nThe Event Team'
+            email_from = settings.EMAIL_HOST_USER
+            recipient_list = [registration.email]  # Use the email from the registration
+            send_mail(subject, message, email_from, recipient_list)
             return redirect('confirmation', registration_id=registration.id)
     else:
         form = RegistrationForm()
